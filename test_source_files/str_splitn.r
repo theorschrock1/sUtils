07d0a92bde37d9cbd7db601a0377d21bea481b39
theordore schrock <theorschrock@gmail.com>
2020-11-30 13:55:53 -0700

    added tests for '{test_name}'

---
+++ test_source_files/str_splitn.r
@@ -0,0 +1,8 @@
+library(sUtils)
+init <- NULL
+str_splitn("#' NOTE: If you manually change your package name in the DESCRIPTION,\n  #' don't forget to change it here too, and in the config file.\n  #' For a safer name change mechanism, use the function.", 
+    trim = TRUE, unlist = TRUE)
+str_splitn("#' NOTE: If you manually change your package name in the DESCRIPTION,\n  #' don't forget to change it here too, and in the config file.\n  #' For a safer name change mechanism, use the function.", 
+    trim = FALSE, unlist = TRUE)
+str_splitn("#' NOTE: If you manually change your package name in the DESCRIPTION,\n  #' don't forget to change it here too, and in the config file.\n  #' For a safer name change mechanism, use the function.", 
+    trim = FALSE, unlist = FALSE)
