709481eb2fcf2a11016c956258bab3e11828f0a4
theordore schrock <theorschrock@gmail.com>
2020-12-04 15:24:34 -0700

    added tests for "capitalize"

---
+++ tests/testthat/_snaps/capitalize.md
@@ -0,0 +1,7 @@
+# capitalize
+
+    Code
+      capitalize(c("documents", "test", "1st"))
+    Output
+      [1] "Documents" "Test"      "1st"      
+
--- tests/testthat/test_capitalize.R
+++ tests/testthat/test_capitalize.R
@@ -1,10 +1,4 @@
-library(sUtils)
-context("capitalize")
-test_that("capitalize(c(\"documents\", \"test\", \"1st\"))", 
-    {
-        output <- {
-            capitalize(c("documents", "test", "1st"))
-        }
-        expect_equal(test_summary(output), list(N = 3L, class = "character", 
-            values = c("Documents", "Test", "1st")))
-    })
+test_that("capitalize", {
+  local_edition(3)
+  expect_snapshot(capitalize(c("documents", "test", "1st")), cran = TRUE)
+})
