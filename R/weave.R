1714aa081b5883e14128b52d32e01aaa0fb95d70
theordore schrock <theorschrock@gmail.com>
2020-12-16 15:48:46 -0700

    added weave.R

---
+++ R/weave.R
@@ -0,0 +1,41 @@
+#' weave to vectors or lists.
+
+#' @name weave
+#' @param x
+#' @param y
+#' @return \code{weave}: \code{[vector|list]}
+#' @examples
+
+#'  weave(letters, LETTERS)
+#'  weave(letters, as.character(1:26))
+#'  weave(letters, '.')
+#'  weave(1:5, 6:9)
+#'  weave(list(1, factor('G'), TRUE), list('k', 'k', 'l'))
+#' @export
+weave <- function(x, y) {
+    # weave to vectors or lists
+    assert(check_vector(x), check_list(x))
+    assert(check_class(y, classes = class(x)), check_length(y, len = c(1, length(x) - 
+        1, length(x))), combine = "and")
+    if (is.list(x)) {
+        x <- lapply(x, list)
+        y <- lapply(y, list)
+    }
+    sameLen = length(y) == length(x)
+    if (sameLen && length(y) == 1) 
+        return(c(x, y))
+    if (length(y) == 1) 
+        y = rep(y, length(x) - 1)
+    ylast = NULL
+    if (sameLen) {
+        ylast = last(y)
+        y = y[1:(length(y) - 1)]
+        if (is.list(x)) 
+            ylast = ylast[[1]]
+    }
+    out = c(reduce2(x, y, function(x, y, sep) {
+        c(x, sep, y)
+    }), ylast)
+    out
+    # Returns: \code{[vector|list]}
+}
