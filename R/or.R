7ee0dfbff34349e2095a928feddfd8607e693430
theodore schrock <theo@consumerdirect.com>
2022-07-10 17:36:50 -0700

    Version bumped from '0.1.0.9022' to '0.1.0.9023'

--- R/or.R
+++ R/or.R
@@ -22,12 +22,14 @@
 #'  x %or% y
 
 #' @export
-"%or%" <-function (x, y)
-{
-  if(missing(x))return(y)
-  if(is.list(x)&&l(x)>0)return(x)
-  if (is.null(x) | l(x) == 0) return(y)
-  if (length(x)==1 &(x[1] == F|x[1] == ""))   return(y)
+"%or%"<-function(x,y){
+  if (missing(x))
+    return(y)
+  if (is.list(x) && l(x) > 0)
+    return(x)
+  if (is.null(x) | l(x) == 0)
+    return(y)
+  if (length(x) == 1 & (x[1] == F | isTRUE(x[1] == "")))
+    return(y)
   return(x)
 }
-
